7e60f898dc8b914c72860fe2f4c4942d244036dc
pandawan69 <41878330+pandawan69@users.noreply.github.com>
2019-09-10 15:55:41 -0300

    Tela relatório do dia

--- app/src/main/java/com/example/aluno/lpb3bim/RelatorioDia.java
+++ app/src/main/java/com/example/aluno/lpb3bim/RelatorioDia.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Relatorio extends AppCompatActivity {
+public class RelatorioDia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relatorio);
+        setContentView(R.layout.activity_relatorio_dia);
     }
 
     public void fechar(View v){
